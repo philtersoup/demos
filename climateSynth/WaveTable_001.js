821db24a800d83d9e2ca3aadcad326400e4c748e
Sanjay Das <Sanjay@Sanjays-MacBook-Pro.local>
2017-09-27 20:58:41 +0100

    add sequencer

--- climateSynth/WaveTable_001.js
+++ climateSynth/WaveTable_001.js
@@ -10,8 +10,11 @@ var scales = [
 
 ];
 
+var counter = 5;
+
 var ac,waveOsc1,waveOsc2,waveOsc3,lpf,verb,delay;
 var analyser, bufferLength, dataArray;
+var part;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
@@ -24,7 +27,7 @@ function setup() {
 
   lpf = new p5.LowPass();
   lpf.freq(500);
-  lpf.amp(0.25);
+  lpf.amp(0.21);
   lpf.disconnect();
   lpf.connect(analyser);
 
@@ -38,27 +41,36 @@ function setup() {
   delay.process(lpf,0.7,0.5);
   delay.drywet(0.5);
 
+  part = new p5.Part(16);
+  part.onStep(randomise);
+  part.setBPM(70);
+
   waveOsc1 = new wtOscillator('JSON/globalTemperature.json',32);
   waveOsc2 = new wtOscillator('JSON/CO2.json',64);
-  waveOsc3 = new wtOscillator('JSON/seaIce.json',32);
+  waveOsc3 = new wtOscillator('JSON/seaIce.json',64);
   // waveOsc3 = new wtOscillator('/JSON/Antarctic_Ice.json');
-  waveOsc1.gain.gain.setvalue = 0.25;
+
+  waveOsc1.gain.gain.setvalue = 0.1;
 
   waveOsc2.gain.disconnect();
   waveOsc2.gain.connect(waveOsc1.osc.frequency);
   waveOsc2.osc.connect(lpf.biquad.frequency);
-  waveOsc2.osc.frequency.value = 1;
-  waveOsc2.gain.gain.value = 1500;
+  // waveOsc2.osc.frequency.value = 1;
+  // waveOsc2.gain.gain.value = 1500;
 
-  waveOsc2.gain.connect(waveOsc3.osc.frequency);
-  waveOsc3.gain.gain.setValue = 0.25;
-  waveOsc3.gain.connect(waveOsc1.osc.frequency);
+  // waveOsc3.gain.disconnect();
+  // waveOsc2.gain.connect(waveOsc3.osc.frequency);
+  // waveOsc3.gain.connect(waveOsc1.gain.gain);
+  waveOsc3.gain.gain.setValue = 5;
+  waveOsc3.gain.connect(waveOsc2.osc.frequency);
+
+  // waveOsc3.gain.connect(waveOsc3.gain.gain);
 
 
 }
 
 function draw() {
-  background(0,20);
+
 
   // fill(255);
   textSize(40);
@@ -67,36 +79,36 @@ function draw() {
 
   analyser.getByteFrequencyData(dataArray);
 
-  for(var i = 0; i < 15; i++){
-    noStroke();
-    fill(dataArray[(i)*40]/2)
-    rect(0,height/(i),width,height/i);
-  }
-
-
+  background(dataArray[40]/5,20);
   // noFill();
   // beginShape();
   // stroke(255,0,0); // waveform is red
   // strokeWeight(1);
   // for (var i = 0; i< dataArray.length; i++){
   //   var x = map(i, 0, dataArray.length, 0, width*8);
-  //   var y = map( dataArray[i], 0, 128, height/2, height/4);
+  //   var y = map( dataArray[i], 0, 128, height, height/3);
   //   vertex(x,y);
   //   // fill(dataArray[i]);
   //   // rect(x,y,width/dataArray.length,height);
   // }
   // endShape();
 
-
+  for(var i = 0; i < 15; i++){
+    noStroke();
+    fill(dataArray[(i)*40]/2)
+    rect(0,height/(i),width,height/i);
+  }
 
   var index = floor((2.71828/3* mouseX/width * scales[scaleNo].length));
   var note = 12 + 12 * floor(2 + (mouseY/height))+scales[scaleNo][index];
 
-  waveOsc2.gain.gain.value = 15000 * mouseY/height;
+  waveOsc2.gain.gain.value = 3000 * (mouseY+1)/height;
   waveOsc1.update(midiToFreq(note + 12));
-  waveOsc2.update(midiToFreq(note + 7 ));
-  waveOsc3.update(midiToFreq(note + 24));
+  waveOsc2.update(midiToFreq( + note + 7 )*counter);
+  // waveOsc3.update(midiToFreq(note -24 ));
+  // waveOsc2.gain.gain.value = 2000 * mouseX/width;
   // waveOsc3.update(midiToFreq(-12 + note + 7));
+  part.start();
 
 }
 
@@ -145,4 +157,10 @@ function wtOscillator(dataSource,len){
 
 wtOscillator.prototype.update = function(freq){
     this.osc.frequency.value = freq;
+}
+
+function randomise(){
+  counter = Math.floor(random(1,5));
+  // console.log(counter);
+
 }
